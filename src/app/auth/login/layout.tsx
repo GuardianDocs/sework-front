e81a8a96d471b0a372df228fc5b7e2752470c6bb
Wan Sim <0420syj@naver.com>
2023-06-29 22:12:02 +0900

    fix: Route별 layout 설정

--- src/app/auth/login/layout.tsx
+++ src/app/auth/login/layout.tsx
@@ -1,18 +1,15 @@
-import '@/app/globals.css';
-import { Inter } from 'next/font/google';
-import { Metadata } from 'next';
-
-const inter = Inter({ subsets: ['latin'] });
-
-const metadata: Metadata = {
-	title: 'Next.js Template',
-	description: 'Next.js template for fast development',
-};
-
-const RootLayout = ({ children }: { children: React.ReactNode }) => (
-	<html lang="en" className="h-full bg-white">
-		<body className={`${inter.className} h-full`}>{children}</body>
-	</html>
-);
-
-export { metadata, RootLayout as default };
+export default function LoginLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<>
+			<div className="min-h-screen bg-gray-100">
+				<div className="flex flex-col items-center pt-6 bg-gray-100 sm:justify-center sm:pt-0">
+					<div>{children}</div>
+				</div>
+			</div>
+		</>
+	);
+}
