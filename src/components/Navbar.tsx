91bda641cca55804daa51f6cbde59a403b3a8b23
Wan Sim <0420syj@naver.com>
2023-10-02 21:11:58 +0900

    refactor: NavBar 컴포넌트

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import useLoginInfoStore from '@/hooks/useLoginInfoStore';
 import Link from 'next/link';
 import { useState } from 'react';
+import useLoginInfoStore from '@/hooks/useLoginInfoStore';
 
 type NavbarLinkProps = {
 	href: string;
@@ -18,9 +18,7 @@ const NavbarLink: React.FC<NavbarLinkProps> = ({ href, children }) => (
 	</Link>
 );
 
-const Navbar = () => {
-	const [menuOpen, setMenuOpen] = useState(false);
-
+const NavbarLinks = () => {
 	const links = [
 		{ href: '/company-info', label: '진단' },
 		{ href: '/pricing', label: '가격' },
@@ -28,10 +26,39 @@ const Navbar = () => {
 		{ href: '/contact', label: '문의하기' },
 	];
 
+	return (
+		<>
+			{links.map(link => (
+				<NavbarLink key={link.href} href={link.href}>
+					{link.label}
+				</NavbarLink>
+			))}
+		</>
+	);
+};
+
+const UserStatus = () => {
 	const { getCompanyName, reset } = useLoginInfoStore(state => ({
 		getCompanyName: state.actions.getCompanyName,
 		reset: state.actions.reset,
 	}));
+
+	return getCompanyName() ? (
+		<>
+			<span className="font-semibold text-slate-700">
+				{getCompanyName()}님
+			</span>
+			<span className="font-semibold text-slate-700" onClick={reset}>
+				로그아웃
+			</span>
+		</>
+	) : (
+		<NavbarLink href="/auth/login">로그인</NavbarLink>
+	);
+};
+
+const Navbar = () => {
+	const [menuOpen, setMenuOpen] = useState(false);
 
 	return (
 		<div className="bg-blue-400">
@@ -52,38 +79,14 @@ const Navbar = () => {
 					</button>
 				</div>
 				<div className="hidden space-x-4 md:flex">
-					{links.map(link => {
-						return (
-							<NavbarLink key={link.href} href={link.href}>
-								{link.label}
-							</NavbarLink>
-						);
-					})}
-					{getCompanyName() ? (
-						<>
-							<span className="font-semibold text-slate-700">
-								{getCompanyName()}님
-							</span>
-							{/* TODO: 로그아웃 기능 구현 */}
-							<span
-								className="font-semibold text-slate-700"
-								onClick={reset}
-							>
-								로그아웃
-							</span>
-						</>
-					) : (
-						<NavbarLink href="/auth/login">로그인</NavbarLink>
-					)}
+					<NavbarLinks />
+					<UserStatus />
 				</div>
 			</nav>
 			{menuOpen && (
 				<div className="flex flex-col py-4 space-y-2 text-center md:hidden">
-					{links.map(link => (
-						<NavbarLink key={link.href} href={link.href}>
-							{link.label}
-						</NavbarLink>
-					))}
+					<NavbarLinks />
+					<UserStatus />
 				</div>
 			)}
 		</div>
