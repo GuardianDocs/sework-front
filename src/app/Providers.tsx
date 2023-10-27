0e19c346137986b403672dc4d66de0dc7b5cf2b7
Wanny.Sim <0420syj@naver.com>
2023-10-27 12:45:40 +0900

    fix: Legacy 페이지 이동

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -4,10 +4,11 @@ import { NextUIProvider } from '@nextui-org/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-	const queryClient = new QueryClient();
-	return (
-		<QueryClientProvider client={queryClient}>
-			<NextUIProvider>{children}</NextUIProvider>
-		</QueryClientProvider>
-	);
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>{children}</NextUIProvider>
+    </QueryClientProvider>
+  );
 }
