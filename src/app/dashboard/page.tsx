d094cb157b8171de4f67afad64be0253b9febb06
Wan Sim <0420syj@naver.com>
2023-11-06 20:27:53 +0900

    바로가기

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <>
+      <Link href="/dashboard/step1">Step 1</Link>
+      <Link href="/dashboard/step2">Step 2</Link>
+      <Link href="/dashboard/step3">Step 3</Link>
+      <Link href="/dashboard/step4">Step 4</Link>
+    </>
+  );
+}
