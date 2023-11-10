be4a3fbae797e3ba7e3cf28ffb26f352d613bb66
Wanny.Sim <0420syj@naver.com>
2023-11-11 02:36:37 +0900

    fix: 스토리북 불필요 애드온 삭제

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -2,17 +2,15 @@ import { render, screen } from '@testing-library/react';
 import Home from '@/app/page';
 
 describe('Home', () => {
-	beforeEach(() => {
-		render(<Home />);
-	});
+  beforeEach(() => {
+    render(<Home />);
+  });
 
-	test('renders HeroSection component', () => {
-		expect(
-			screen.getByText('Intelligent Risk Assurance System'),
-		).toBeInTheDocument();
-	});
+  test('renders HeroSection component', () => {
+    expect(screen.getByText('Intelligent Risk Assurance System')).toBeInTheDocument();
+  });
 
-	test('1+1=2', () => {
-		expect(1 + 1).toBe(2);
-	});
+  test('1+1=2', () => {
+    expect(1 + 1).toBe(2);
+  });
 });
