dbe39db7f5699a8c550a7e4497e21d66ffd28027
Wanny.Sim <0420syj@naver.com>
2023-11-11 02:54:02 +0900

    fix: 아이콘 스토리 개선

--- src/components/ui/Icon/Icon.stories.ts
+++ src/components/ui/Icon/Icon.stories.ts
@@ -1,11 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Icon from './Icon';
+import Icon, { iconKeys } from './Icon';
+
+const iconOptions = iconKeys;
+const iconMapping = iconOptions.reduce((acc: Record<string, string>, key) => {
+  acc[key] = key;
+  return acc;
+}, {});
 
 const meta: Meta<typeof Icon> = {
   title: 'Design System/UI/Icon',
   component: Icon,
   tags: ['autodocs'],
+  argTypes: {
+    icon: {
+      options: iconOptions,
+      mapping: iconMapping,
+      control: { type: 'select' },
+    },
+  },
 };
 
 export default meta;
