6dca29b5bdff91493d45c8449b6566df899f975c
Wanny.Sim <0420syj@naver.com>
2023-11-24 21:39:31 +0900

    api 모듈 구현

---
+++ src/lib/oas-axios.ts
@@ -0,0 +1,39 @@
+import * as OpenApiInstance from '@/services';
+
+const { NODE_ENV } = process.env;
+
+const getBasePath = () => {
+  switch (NODE_ENV) {
+    case 'development':
+      return 'https://api-dev.iras.kr';
+    case 'production':
+      return 'https://api.iras.kr';
+    default:
+      return 'https://api-dev.iras.kr';
+  }
+};
+
+// get localStroage
+const loginInfoLocalStorage = localStorage.getItem('loginInfo');
+const loginInfo = loginInfoLocalStorage ? JSON.parse(loginInfoLocalStorage) : null;
+
+// get accessToken
+const accessToken = loginInfo?.state?.accessToken;
+
+const apiConfigurationParams: OpenApiInstance.ConfigurationParameters = {
+  basePath: getBasePath(),
+  baseOptions: {
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  },
+};
+
+const apiConfiguration: OpenApiInstance.Configuration = new OpenApiInstance.Configuration(apiConfigurationParams);
+
+export const TestApi = new OpenApiInstance.TestControllerApi(apiConfiguration);
+export const Step0Api = new OpenApiInstance.DefaultApi(apiConfiguration);
+export const Step1Api = new OpenApiInstance.Class1Api(apiConfiguration);
+export const Step2Api = new OpenApiInstance.Class2Api(apiConfiguration);
+export const Step34Api = new OpenApiInstance.Class34Api(apiConfiguration);
