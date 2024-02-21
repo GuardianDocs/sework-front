367d7f3618262f929971c15e5869f3de68b27d5b
Tom <godppun@naver.com>
2024-02-21 21:43:52 +0900

    feat: login 페이지 layout 추가

---
+++ src/app/auth/login/layout.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { PropsWithChildren } from 'react';
+import loginGateImage from './img-login_gate.svg';
+
+export default function LoginLayout({ children }: PropsWithChildren) {
+  return (
+    <div className="w-[100vw] h-[100vh] flex">
+      <div className="h-full min-w-[810px] bg-blue-100 w-[810px] flex items-center justify-center relative">
+        <Link href="/">
+          <Image src="/main-logo.png" width={90} height={30} alt="logo" className="absolute top-12 left-[60px]" />
+        </Link>
+        <Image src={loginGateImage} alt="login gate" width={500} height={400} />
+      </div>
+      <div className="flex-1 self-stretch flex items-center justify-center">{children}</div>
+    </div>
+  );
+}
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,18 +1,10 @@
-import type { Metadata } from 'next';
 import LoginPage from '@/components/page/login/LoginPage';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: '로그인',
 };
 
 export default function Page() {
-  return (
-    <>
-      <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <LoginPage />
-        </div>
-      </div>
-    </>
-  );
+  return <LoginPage />;
 }
