6df7c2a3a181639ca644565b41058b3a6be2ba6c
Tom <godppun@naver.com>
2024-02-27 17:57:02 +0900

    fix: 평가 버전 선택 카드 스타일 수정

--- src/components/ui/CardButton/CardButton.tsx
+++ src/components/ui/CardButton/CardButton.tsx
@@ -16,16 +16,16 @@ export const CardButton = ({ icon, title, subContents, updatedAt, actived, ...pr
     <button
       className={cn(
         'transition-all text-start w-full p-6 flex gap-x-3 items-center rounded-lg border border-gray-100 hover:border-blue-200',
-        actived && 'border-blue-500 hover:border-blue-500'
+        actived && 'border-blue-500 hover:border-blue-500 bg-blue-50'
       )}
       {...props}
     >
       {icon && (
-        <div className="flex-center w-9 h-9 bg-blue-50 rounded-full">
-          <Icon icon={icon} className="text-gray-800" />
+        <div className="flex-center w-9 h-9 bg-blue-200 rounded-full">
+          <Icon icon={icon} className="text-white" />
         </div>
       )}
-      <div className="flex flex-col gap-y-2">
+      <div className="flex-1 flex flex-col gap-y-2">
         <Title size="l" color="gray800">
           {title}
         </Title>
@@ -33,13 +33,14 @@ export const CardButton = ({ icon, title, subContents, updatedAt, actived, ...pr
           <div className="flex flex-col gap-y-1">
             {subContents}
             {updatedAt && (
-              <Body size="s" color="gray400">
+              <Body size="s" color="gray500">
                 {updatedAt}
               </Body>
             )}
           </div>
         )}
       </div>
+      {actived && <Icon icon="check" className="text-blue-500 w-8 h-8" />}
     </button>
   );
 };
